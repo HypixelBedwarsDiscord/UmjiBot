f344f566900ff5e54b12b9c9147eb780fbfee099
myerfire <myercheng0614@gmail.com>
2021-09-04 14:08:22 -0400

    delete messages from verified member in verification channel

--- commands/bwverify.js
+++ commands/bwverify.js
@@ -68,5 +68,9 @@ module.exports = {
                 { name: "Minecraft IGN", value: player.nickname }
             );
         await interaction.guild.channels.cache.get(verificationLogsChannelID).send({ embeds: [logEmbed] });
+        // delete messages from verified member in verification channel
+        interaction.guild.channels.cache.get(verificationChannelID).messages.fetch().then(async messages => {
+            await interaction.guild.channels.cache.get(verificationChannelID).bulkDelete(messages.filter(m => m.author.id === interaction.member.id));
+        });
     }
 };
