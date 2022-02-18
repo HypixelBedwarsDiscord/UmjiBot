1d6561cafa20d2ca39cbff73f69149b6589652fc
myer <myercheng0614@gmail.com>
2022-02-18 04:33:48 +0000

    updated the detection so it's not broken xd

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "messageCreate",
     execute(message) {
-        if (!message.nonce) {
-            console.log(`[events/messageCreate.js] ${message.author.name}#${message.author.discriminator}: ${message.content} | nonce: ${message.nonce}`);
+        if (!message.nonce && !message.author.bot) {
+            console.log(`[events/messageCreate.js] #${message.channel.name}: ${message.author.username}#${message.author.discriminator}: ${message.content} | nonce: ${message.nonce}`);
         }
     }
 };
